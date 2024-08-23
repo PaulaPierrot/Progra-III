2e50796ee64549fd96e71b97b7a138bc9d10789a
chiara-p581 <chiaraagustina19@gmail.com>
2024-08-23 16:27:41 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
-public class Main {
-    public static void main(String[] args) {
+public class Clase1Actividad1 {
+    public static void Clase1Actividad1(String[] args) {
         int [] array = {1,2,3}; //1
         int mayor = 0;  //1
 
